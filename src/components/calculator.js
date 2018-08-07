e4cea17ea3ac9da765ea3137b0211e465e4af7a6
Code <Code@batc.local>
2018-08-07 09:02:46 -0600

    added font awesome link, repostitioned buttons to fix calculator breaking in full screen, and centered the calculator on screen)

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -13,15 +13,15 @@ export default class Calculator extends Component {
               <button className="calculator__button__percentage-key" label="%" value="percentage">%</button>
             </div>
             <div className="calculator__digit-keys">
-              <button className="calculator__button__key-1" label="1" value="1">1</button>
-              <button className="calculator__button__key-2" label="2" value="2">2</button>
-              <button className="calculator__button__key-3" label="3" value="3">3</button>
-              <button className="calculator__button__key-4" label="4" value="4">4</button>
-              <button className="calculator__button__key-5" label="5" value="5">5</button>
-              <button className="calculator__button__key-6" label="6" value="6">6</button>
               <button className="calculator__button__key-7" label="7" value="7">7</button>
               <button className="calculator__button__key-8" label="8" value="8">8</button>
               <button className="calculator__button__key-9" label="9" value="9">9</button>
+              <button className="calculator__button__key-4" label="4" value="4">4</button>
+              <button className="calculator__button__key-5" label="5" value="5">5</button>
+              <button className="calculator__button__key-6" label="6" value="6">6</button>
+              <button className="calculator__button__key-1" label="1" value="1">1</button>
+              <button className="calculator__button__key-8" label="2" value="2">2</button>
+              <button className="calculator__button__key-9" label="3" value="3">3</button>
               <button className="calculator__button__key-0" label="0" value="0">0</button>
               <button className="calculator__button__key-dot" label="." value=".">.</button>
             </div>
